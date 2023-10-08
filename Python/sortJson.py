79222e0e39a7231aa964544ba9c9e2261db4dec6
Kojiro23 <Kojiro23>
2023-10-08 17:23:22 +0900

    Improve:JsonSort

--- Python/sortJson.py
+++ Python/sortJson.py
@@ -1,5 +1,8 @@
 from collections import OrderedDict
+from deepdiff import DeepDiff
 import json
+import os
+import difflib
 
 def sort_jsonFile(json1, json2):
     with open(json1, encoding='utf-8') as file1, open(json2, encoding='utf-8') as file2:
@@ -9,5 +12,16 @@ def sort_jsonFile(json1, json2):
         sorted_json2 = OrderedDict(sorted(data2.items(), key=lambda x: x[0]))
         json.dump(sorted_json1, open('data1.json', 'w', encoding='utf-8'), ensure_ascii=False, indent=4)
         json.dump(sorted_json2, open('data2.json', 'w', encoding='utf-8'), ensure_ascii=False, indent=4)
+        showDiff()
+
+def showDiff():
+    with open('data1.json', 'r') as f1, open('data2.json', 'r') as f2:
+        json1 = json.load(f1)
+        json2 = json.load(f2)
+
+        diff = DeepDiff(json1, json2)
+
+        # print(diff)
+        print(diff.to_json(indent=4))
 
 sort_jsonFile('json1.json', 'json2.json')
